506b2ecf0d4e994faccd5be7ff9e45d3a8a3595a
William Harrison <wharrison@horizontal.com>
2024-02-24 08:51:08 -0500

    adds sanity

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '7m8frgh6',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import blockContent from './blockContent'
+import category from './category'
+import painting from './painting'
+import post from './post'
+import author from './author'
+
+export const schemaTypes = [painting, post, author, category, blockContent]
