3f66794208607025f8afbae6d92cb0ad2813af2b
William Harrison <wm.harrison@gmail.com>
2024-02-24 09:24:46 -0500

    updates index, untracks nvms

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import { sanityIntegration } from "@sanity/astro";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
     dataset: 'production',
     apiVersion: '2023-02-08',
     useCdn: false
-  })],
+  }), tailwind()],
   image: {
     domains: ['cdn.sanity.io']
   }
